26151accbb887b30f25bd84606d19edd67db5cae
hydra324 <akhilchilumuru@gmail.com>
2020-04-29 22:50:20 +0530

    refactor: firebase config no longer needs absolute path

--- litmus-messaging/src/main/java/com/fiscord/litmus/litmus_messaging/firebase/authentication/FirebaseSDKAuthentication.java
+++ litmus-messaging/src/main/java/com/fiscord/litmus/litmus_messaging/firebase/authentication/FirebaseSDKAuthentication.java
@@ -1,5 +1,6 @@
 package com.fiscord.litmus.litmus_messaging.firebase.authentication;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.util.ResourceUtils;
 
 
 @Service
@@ -20,7 +22,9 @@ public class FirebaseSDKAuthentication {
 	public void initiliaze() {
 		try {
 			System.out.println("Initializing Firebase");
-			FileInputStream serviceAccount = new FileInputStream("C://tmp/litmus-1b878-firebase-adminsdk.json");
+
+			File file = ResourceUtils.getFile("classpath:/config/firebase/litmus-1b878-firebase-adminsdk.json");
+			FileInputStream serviceAccount = new FileInputStream(file);
 
 			FirebaseOptions options = new FirebaseOptions.Builder()
 					.setCredentials(GoogleCredentials.fromStream(serviceAccount))
@@ -36,6 +40,6 @@ public class FirebaseSDKAuthentication {
 		}
 		System.out.println("Firebase Initialized");
 	}
-	
+
 
 }
