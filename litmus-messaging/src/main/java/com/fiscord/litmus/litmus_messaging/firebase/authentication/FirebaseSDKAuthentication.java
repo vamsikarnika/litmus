02701e21ffb11ceca62cd645d4eaaee528e2e71a
hydra324 <akhilchilumuru@gmail.com>
2020-05-01 03:39:52 +0530

    fix: fixed resource fetching for firebase key

--- litmus-messaging/src/main/java/com/fiscord/litmus/litmus_messaging/firebase/authentication/FirebaseSDKAuthentication.java
+++ litmus-messaging/src/main/java/com/fiscord/litmus/litmus_messaging/firebase/authentication/FirebaseSDKAuthentication.java
@@ -1,18 +1,17 @@
 package com.fiscord.litmus.litmus_messaging.firebase.authentication;
 
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.InputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import org.springframework.util.ResourceUtils;
 
 
 @Service
@@ -23,8 +22,7 @@ public class FirebaseSDKAuthentication {
 		try {
 			System.out.println("Initializing Firebase");
 
-			File file = ResourceUtils.getFile("classpath:/config/firebase/litmus-1b878-firebase-adminsdk.json");
-			FileInputStream serviceAccount = new FileInputStream(file);
+            InputStream serviceAccount = new ClassPathResource("config/firebase/litmus-1b878-firebase-adminsdk.json").getInputStream();
 
 			FirebaseOptions options = new FirebaseOptions.Builder()
 					.setCredentials(GoogleCredentials.fromStream(serviceAccount))
